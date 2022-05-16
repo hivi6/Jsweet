3f7cbfb15ab7da42e26f74e9c9b29cdb579e30c9
hivi6 <tthreehivi@gmail.com>
2022-05-17 03:38:13 +0530

    Added simple error handling

--- src/runtime/SweetRuntime.java
+++ src/runtime/SweetRuntime.java
@@ -8,6 +8,35 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class SweetRuntime {
+    // =========
+    // Error Handling
+    // =========
+
+    private static boolean hadError = false;
+
+    public static void error(int line, int column, String msg) {
+        report(line, column, "", msg);
+    }
+
+    public static void error(int line, String msg) {
+        report(line, "", msg);
+    }
+
+    private static void report(int line, int column, String where, String msg) {
+        // Format: [Line 13:14] Error: 
+        System.err.println("[" + line + ": " + column + "] Error" + where + ": " + msg);
+        hadError = true;
+    }
+
+    private static void report(int line, String where, String msg) {
+        System.err.println("[Line " + line + "] Error" + where + ": " + msg);
+        hadError = true;
+    }
+
+    // ==========
+    // Application interface
+    // ==========
+
     // repl - read evaluate print loop
     public static void runRepl() throws IOException {
         // make an instance of reader
@@ -26,6 +55,9 @@ public class SweetRuntime {
             
             // run
             run(source);
+
+            // reset any errors to rerun the loop
+            hadError = false;
         }
     }
 
@@ -36,6 +68,10 @@ public class SweetRuntime {
         
         // run
         run(source);
+
+        // indicating error in exit mode
+        if (hadError)
+            System.exit(65);
     }
 
     private static void run(String source) {
